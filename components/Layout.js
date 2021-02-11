9f5012d0f8aa435f62309fce56c4f0478c12fcf4
KEI <68884990+kei1992@users.noreply.github.com>
2021-02-11 16:00:52 +0900

    fetch-all-blog

--- components/Layout.js
+++ components/Layout.js
@@ -27,9 +27,9 @@ export default function Layout({ children, title = "This is Blog App" }) {
                     aria-hidden="true"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M4 6h16M4 12h16M4 18h16"
                     />
                   </svg>
@@ -39,12 +39,12 @@ export default function Layout({ children, title = "This is Blog App" }) {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    aria-hidden="true"
+                    ariaHidden="true"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M6 18L18 6M6 6l12 12"
                     />
                   </svg>
@@ -135,7 +135,7 @@ export default function Layout({ children, title = "This is Blog App" }) {
         </nav>
       </header>
 
-      <main classNameName="flex-grow">{children}</main>
+      <main className="flex-grow">{children}</main>
       <footer className="w-full h-12 bg-gray-800 text-white flex justify-center items-center">
         Blog App
       </footer>
--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,13 +1,23 @@
 import Layout from "../components/Layout";
+import { getAllPostsData } from "../lib/posts";
+import Post from "../components/Post";
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
     <Layout title="Blog">
       <div className="min-h-screen px-2 py-0 flex justify-center items-center">
-        <h1>Blog Page</h1>
+        <ul className="m-10 text-black">
+          {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+        </ul>
       </div>
     </Layout>
   );
 };
 
 export default Blog;
+
+// getStaticPropsは、ビルド時に実行される。
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return { props: { posts } };
+}
